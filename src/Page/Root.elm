cac28732f3f2fbb6afaef44bd8748a6c3b61ce35
rotiuskyi <roman.otiuskyi@gmail.com>
2021-04-28 11:05:42 +0300

    generate random pairs

--- src/Page/Root.elm
+++ src/Page/Root.elm
@@ -1,37 +1,46 @@
 module Page.Root exposing (..)
 
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, li, text, ul)
 import Html.Events exposing (onClick)
 import Random
 
 
 type alias RootPgModel =
-    Int
+    List ( Int, Int )
 
 
 init : RootPgModel
 init =
-    0
+    []
 
 
 type RootPgMsg
-    = GenerateRandNumb
-    | GeneratedRandNumb Int
+    = GenerateRandomPairs
+    | GeneratedRandomPairs (List ( Int, Int ))
+
+
+generatePairs : Cmd RootPgMsg
+generatePairs =
+    let
+        randNum =
+            Random.int 0 5
+    in
+    Random.generate GeneratedRandomPairs <| Random.list 10 <| Random.pair randNum randNum
 
 
 update : RootPgMsg -> RootPgModel -> ( RootPgModel, Cmd RootPgMsg )
 update msg model =
     case msg of
-        GenerateRandNumb ->
-            ( model, Random.generate GeneratedRandNumb (Random.int 0 10) )
+        GenerateRandomPairs ->
+            ( model, generatePairs )
 
-        GeneratedRandNumb numb ->
-            ( numb, Cmd.none )
+        GeneratedRandomPairs pairs ->
+            ( pairs, Cmd.none )
 
 
 view : RootPgModel -> Html RootPgMsg
 view model =
     div []
-        [ button [ onClick GenerateRandNumb ] [ text "Generate Number" ]
-        , text <| String.fromInt model
+        [ button [ onClick GenerateRandomPairs ] [ text "Generate Pairs" ]
+        , ul [] <| List.map (\( n0, n1 ) -> li [] [ text <| String.fromInt n0 ++ "-" ++ String.fromInt n1 ]) <| model
         ]
