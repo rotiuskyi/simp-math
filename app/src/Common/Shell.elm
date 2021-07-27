72cd23dcadefda6d98cf8af1579c185e00bd8df4
rotiuskyi <roman.otiuskyi@gmail.com>
2021-07-27 15:24:21 +0300

    styles changes, add footer

--- app/src/Common/Shell.elm
+++ app/src/Common/Shell.elm
@@ -1,21 +1,30 @@
 module Common.Shell exposing (view)
 
-import Html exposing (Html, div, img)
+import Html exposing (Html, div, img, small, text)
 import Html.Attributes exposing (class, src)
 
 
 header : Html msg
 header =
-    div [ class "header" ]
+    div [ class "shell__header" ]
         [ div [ class "container logo-box" ]
             [ img [ class "logo-box__logo", src "/assets/img/favicon.ico" ] []
             ]
         ]
 
 
+footer : Html msg
+footer =
+    div [ class "shell__footer" ]
+        [ div [ class "container" ]
+            [ small [] [ text "Created by Roman Otiuskyi" ] ]
+        ]
+
+
 view : (a -> msg) -> Html a -> Html msg
 view toMsg content =
-    div []
+    div [ class "shell" ]
         [ header
-        , Html.map toMsg content
+        , Html.map toMsg <| div [ class "shell__content" ] [ content ]
+        , footer
         ]
